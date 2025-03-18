0bfd9e731a92dd7488235d7235db725115fd78e8
nitishbhardwaj1279 <bhardwajnitish1270@gmail.com>
2025-03-18 14:08:02 -0400

    completed payment and stripe integration

---
+++ src/app/(protected)/payment/loading.tsx
@@ -0,0 +1,10 @@
+import Loader from "@/components/global/loader"
+const Loading=()=>{
+    return (
+        <div className='h-screen flex justify-center items-center'>
+        <Loader state>...</Loader>
+        </div>
+
+    )
+}
+export default Loading
--- src/components/global/payment-button/index.tsx
+++ src/components/global/payment-button/index.tsx
@@ -1,25 +1,25 @@
 import { Button } from '@/components/ui/button'
-// import { useSubscription } from '@/hooks/use-subscription'
+ import { useSubscription } from '@/hooks/use-Subscription'
 import { CreditCardIcon, Loader2 } from 'lucide-react'
 import React from 'react'
 
 type Props = {}
 
 const PaymentButton = (props: Props) => {
-  // const { onSubscribe, isProcessing } = useSubscription()
+   const { onSubscribe, isProcessing } = useSubscription()
   return (
     <Button
-      // disabled={isProcessing}
-      // onClick={onSubscribe}
+       disabled={isProcessing}
+        onClick={onSubscribe}
       className="bg-gradient-to-br
      text-white 
      rounded-full 
-    from-[#6d60a3] 
+    from-[#6f5db8] 
     via-[#9434E6] 
     font-bold 
     to-[#CC3BD4]"
     >
-      {/* {isProcessing ? <Loader2 className="animate-spin" /> : <CreditCardIcon />} */}
+      {isProcessing ? <Loader2 className="animate-spin" /> : <CreditCardIcon />} 
       Upgrade
     </Button>
   )
